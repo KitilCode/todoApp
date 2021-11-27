38e3b10e57c79ff9c368617081a4cac9d28497d2
Dusk Kitil <duskkitil@gmail.com>
2021-11-27 13:29:50 -0500

    added published version of update

--- bin/flowUpdate.js
+++ bin/flowUpdate.js
@@ -5,11 +5,15 @@ require('dotenv').config();
 console.log(process.env.FLOW_SID);
 //console.log(client);
 
-const update = 
+let update = 
     client.studio.flows(process.env.FLOW_SID)
                  .update({commitMessage: 'update create list redirect', definition: flowObject, status: 'draft'})
                  .then(flow => console.log(flow.friendlyName));
 
+update = 
+    client.studio.flows(process.env.FLOW_SID)
+                 .update({commitMessage: 'update create list redirect', definition: flowObject, status: 'published'})
+                 .then(flow => console.log(flow.friendlyName));
 
 // debugging
 // console.log(update);
