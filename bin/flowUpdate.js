498ff9f2b2f0cd10f3289edb996c37f965a35f0c
Dusk Kitil <duskkitil@gmail.com>
2021-11-27 13:16:15 -0500

    fixed flowUpdate.js

--- bin/flowUpdate.js
+++ bin/flowUpdate.js
@@ -2,9 +2,25 @@ const {client} = require('../twilioConstants');
 const flowObject = require('../twilioFlowObject');
 require('dotenv').config();
 
+console.log(process.env.FLOW_SID);
+//console.log(client);
+
 const update = 
     client.studio.flows(process.env.FLOW_SID)
-                 .update({commitMessage: 'update welcome redirect', definition: flowObject, status: 'published'})
+                 .update({commitMessage: 'update create list redirect', definition: flowObject, status: 'draft'})
                  .then(flow => console.log(flow.friendlyName));
+
+
+// debugging
+// console.log(update);
+// console.log(
+//     client.studio.flows(process.env.FLOW_SID)
+//                  .update({commitMessage: 'update create list redirect', definition: flowObject, status: 'published'})
+//                  .then(flow => console.log(flow))
+// );
+
+// client.studio.flows(process.env.FLOW_SID)
+//              .fetch()
+//              .then(flow => console.log(flow.friendlyName));
 
 module.exports = update;
