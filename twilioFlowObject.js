7de4d1c740706d27b91803b8e26df4b738b188c4
duskkitil <88149010+duskkitil@users.noreply.github.com>
2021-11-27 13:54:48 -0500

    Bug/issue 14 fix flow update.js (#17)
    * fixed flowUpdate.js
    
    * added published version of update
    
    * added url parameter to create_list
    
    * fixed parsing of states array
    
    * variables point to proper place in array now

--- twilioFlowObject.js
+++ twilioFlowObject.js
@@ -10,11 +10,13 @@ let optionsSplit = flowObject.states[4];
 let isUserActive = flowObject.states[5];
 let isUserActiveSplit = flowObject.states[6];
 let noSuchOption = flowObject.states[7];
-let createListTitle = flowObject.states[8];
-let createList = flowObject.states[9];
+let optionsFail = flowObject.states[8]
+let createListTitle = flowObject.states[9];
+let createList = flowObject.states[10];
 
 // edit widgets 
 welcome.properties.url = ngrokURL + '/welcome';
-createList.properties.url = ngrokURL + 'todoList/create';
+createList.properties.url = ngrokURL + '/todoList/create';
+
 
 module.exports = flowObject;
