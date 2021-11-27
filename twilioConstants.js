34c6911e774116598590379dff8340a8d8bb1aa5
Dusk Kitil <duskkitil@gmail.com>
2021-11-27 12:54:10 -0500

    edit flow realted files to accomodatee create list route

--- twilioConstants.js
+++ twilioConstants.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 const client = require('twilio')(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TOKEN);
 const MessagingResponse = require('twilio').twiml.MessagingResponse;
-const ngrokURL = 'https://253c-2603-6010-8d02-cf91-896e-1080-7673-498c.ngrok.io';
+const ngrokURL = 'https://b21a-2603-6010-8d02-cf91-404a-c3ea-12ee-f331.ngrok.io';
 const redirectURL = `https://webhooks.twilio.com/v1/Accounts/${process.env.TWILIO_ACCOUNT_SID}/Flows/${process.env.FLOW_SID}?FlowEvent=return`;
 
 module.exports = {
