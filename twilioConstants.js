3a233781386b42a92f1775b8faee07eb311f53f2
Dusk Kitil <duskkitil@gmail.com>
2021-12-01 11:09:52 -0500

    route configured incorrectly

--- twilioConstants.js
+++ twilioConstants.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 const client = require('twilio')(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TOKEN);
 const MessagingResponse = require('twilio').twiml.MessagingResponse;
-const ngrokURL = 'https://b21a-2603-6010-8d02-cf91-404a-c3ea-12ee-f331.ngrok.io';
+const ngrokURL = 'https://6127-2603-6010-8d02-cf91-d511-7fcf-6214-b619.ngrok.io';
 const redirectURL = `https://webhooks.twilio.com/v1/Accounts/${process.env.TWILIO_ACCOUNT_SID}/Flows/${process.env.FLOW_SID}?FlowEvent=return`;
 
 module.exports = {
